168226577760ec975ab14375202b093efe717047
Your Name <your_email@whatever.com>
2023-11-08 11:09:47 +0700

    Рефакторинг кода представлений

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -55,28 +55,3 @@ class SnippetDetail(APIView):
        snippet.delete()
        return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None) :
-   """
-   Retrieve, update or delete a code snippet.
-   """
-   try:
-       snippet = Snippet.objects.get(pk=pk)
-   except Snippet.DoesNotExist:
-       return Response(status=status.HTTP_404_NOT_FOUND)
-
-   if request.method == 'GET':
-       serializer = SnippetSerializer(snippet)
-       return Response(serializer.data)
-
-   elif request.method == 'PUT':
-       serializer = SnippetSerializer(snippet, data=request.data)
-       if serializer.is_valid():
-           serializer.save()
-           return Response(serializer.data)
-       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-   elif request.method == 'DELETE':
-       snippet.delete()
-       return Response(status=status.HTTP_204_NO_CONTENT)
