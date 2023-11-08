dc599a185f91167885433a7bb5ed02d5d9523bcb
Your Name <your_email@whatever.com>
2023-11-08 12:34:48 +0700

    Добавление представлений для пользователя

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-
 from .models import Snippet
-
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
    class Meta:
        model = Snippet
        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.ModelSerializer):
+   snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+   class Meta:
+       model = User
+       fields = ['id', 'username', 'snippets']
