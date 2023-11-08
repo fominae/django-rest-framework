82e0b09bdfb108d6909ef27c267304b13e0d8a26
Your Name <your_email@whatever.com>
2023-11-08 12:41:34 +0700

    Добавление необходимых разрешений

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,11 +3,12 @@ from rest_framework import generics
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
-
+from rest_framework import permissions
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
    def perform_create(self, serializer):
        serializer.save(owner=self.request.user)
@@ -16,6 +17,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class UserList(generics.ListAPIView):
    queryset = User.objects.all()
